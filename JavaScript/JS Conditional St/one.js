91168d260d4a1bb9a538832e0d9c6f87be6ab019
Gokul-Raj-Jerry <gokulraj.jerry@gmail.com>
2022-07-09 22:16:40 -0800

    test

---
+++ JavaScript/JS Conditional St/one.js
@@ -0,0 +1,9 @@
+//WAP 8. Write a program to print the least number given two numbers?
+let a = 23 
+let b = 3000
+if(a>b){
+    console.log("Yes", a , "is Bigger");
+}
+else {
+    console.log(b, "is Bigger")
+}
---
+++ JavaScript/JS Iterative St/one.js
@@ -0,0 +1,7 @@
+// 1. Write a program to print 1 to 10 numbers using a while loop
+
+let q = 1
+while (q <= 10) (
+    console.log(q);
+q = q + 1;
+)
