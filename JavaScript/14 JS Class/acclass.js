83378d159f1b03042906846bd8061a69b840dcaf
Gokul Raj Jerry <gokulraj.jerry@gmail.com>
2022-09-25 17:57:22 -0800

    'test'

---
+++ JavaScript/14 JS Class/acclass.js
@@ -0,0 +1,22 @@
+class Emp {
+  a = 10
+  getEmployee() {
+    console.log("Hello");
+  }
+}
+
+let a1 = new Emp()
+
+//how to access class members
+console.log(a1.a); //how to access class property
+a1.getEmployee() //how to access class methods
+
+//this.keyword
+
+class Account{
+  min_bal = 500;
+  this.min_bal =
+}
+
+new Account;
+
--- JavaScript/2 JS Conditional St/test.js
+++ JavaScript/2 JS Conditional St/test.js
@@ -1,34 +1,34 @@
-// JavaScript Conditional Statement Assignments 
+// JavaScript Conditional Statement Assignments
 
 // 1. Write a program to check if a number is divisible by 7 or not?
 
-let q = 21
+/* let q = 21
 if (q % 7 == 0) {
     console.log("Divisible");
 }
 else {
     console.log("Not Divisible");
-}
+} */
 
 // 2. Write a program to check if a number is multiple of 3 or not?
 
-let w = 9
+/* let w = 9
 if (w % 3 == 0) {
     console.log("Yes Divisible");
 }
 else {
     console.log("Not Divisible");
-}
+} */
 
 // 3. Write a program to check if a number is positive or not?
 
-let e = -5
+/* let e = -5
 if (e < 0 == 0) {
     console.log("Positive");
 }
 else {
     console.log("Negative");
-}
+} */
 
 // 4. Write a program to check if a number is having 4 at the unit's place or not?
 
@@ -36,38 +36,37 @@ else {
 
 // 5. Write a program to check if a number is 10-digit numbers or not?
 
-let t = 9876543810;
+let t = 9876543810
 if (t.toString().length == 10) {
-    console.log("Accepted");
-}
-else {
-    console.log("Not Accepted");
+  console.log('Accepted')
+} else {
+  console.log('Not Accepted')
 }
 
 // 6. Write a program to print even or odd for a given number?
 
-let y = 652
+/* let y = 652
 if (y % 2 == 0) {
     console.log("Even");
 }
 else {
     console.log("Odd");
-}
+} */
 
 // 7. Write a program to print the greatest number in given two numbers?
 
-let u = 5
+/* let u = 5
 let i = 8
 if (u > i) {
     console.log(u);
 }
 else {
     console.log(i);
-}
+} */
 
 // 8. Write a program to print the least number given two numbers?
 
-let o = 1
+/* let o = 1
 let p = 2
 if (o < p) {
     console.log(o);
@@ -75,10 +74,10 @@ if (o < p) {
 else {
     console.log(p);
 }
-
+ */
 // 9. Write a program to print the greatest number in given three numbers?
 
-let a = 54
+/* let a = 54
 let s = 45
 let d = 87
 if (a > s && a > d) {
@@ -89,11 +88,11 @@ else if (s > a && s > d) {
 }
 else {
     console.log("d is Greater");
-}
+} */
 
 // 10. Write a program to print the least number in given three numbers?
 
-let f = 54
+/* let f = 54
 let g = 45
 let h = 87
 if (f > g && f > h) {
@@ -104,23 +103,23 @@ else if (g > f && g > h) {
 }
 else {
     console.log("h is least");
-}
+} */
 
 // 11. Write a program to print the given 3 numbers in ascending orders?
 
-let j=[9,5,7,4,6,2,1]
+/* let j=[9,5,7,4,6,2,1]
 j.sort()
-console.log(j);
+console.log(j); */
 
 // 12. Write a program to print the given 3 numbers in descending orders?
 
-let k=[2,5,1,4,8,9,7]
+/* let k=[2,5,1,4,8,9,7]
 k.sort().reverse()
-console.log(k);
+console.log(k); */
 
 // 13. JavaScript Program to find largest of Three Numbers?
 
-let l = 54
+/* let l = 54
 let z = 45
 let x = 87
 if (l > z && l > x) {
@@ -132,4 +131,4 @@ else if (z > l && z > x) {``
 else {
     console.log("x is largest");
 }
-
+ */
