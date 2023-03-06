3ace93c38fc94bfcc8afbe6aed6fe1ab5a1c0a4a
Gokul Raj Jerry <gokulraj.jerry@gmail.com>
2023-03-06 14:06:31 +0530

    test

--- Fullstack online/JavaScript/DBmongo/app-backup.js
+++ Fullstack online/JavaScript/DBmongo/app-backup.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 
+
 const app = express()
 dotenv.config({ path: './config/config.env' })
 
@@ -17,8 +18,10 @@ app.get('/user/login', (req, res) => {
 })
 //user Regestration
 app.get('/user/reg', (req, res) => {
-    res.send('<h1>user registration</h1>')
+    res.send('<h1>user regestration</h1>')
 })
+
+
 
 
 
---
+++ Fullstack online/JavaScript/DBmongo/userRouter.js
@@ -0,0 +1,8 @@
+import { Express, Router } from "express";
+
+Router.get('/', () => { })
+Router.get('/user', () => { })
+Router.get('/user/login', () => { })
+Router.get('/reg', () => { })
+
+module.exports
