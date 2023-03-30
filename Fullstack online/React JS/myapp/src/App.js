584f4d51a53943db4782f6a0ef4cf534b12b68bd
Gokul Raj Jerry <gokulraj.jerry@gmail.com>
2023-03-31 01:13:51 +0530

    'r'

--- Fullstack online/React JS/myapp/src/App.js
+++ Fullstack online/React JS/myapp/src/App.js
@@ -1,24 +1,25 @@
 // main file
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import About from "./Routing & Navigation/Pages/About";
-import Contact from "./Routing & Navigation/Pages/Contact";
-import Home from "./Routing & Navigation/Pages/Home";
-import Navbar from "./Routing & Navigation/Pages/Navbar";
-import Product from "./Routing & Navigation/Pages/Product";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import About from "./Routing & Navigation/Pages/About";
+// import Contact from "./Routing & Navigation/Pages/Contact";
+// import Home from "./Routing & Navigation/Pages/Home";
+// import Navbar from "./Routing & Navigation/Pages/Navbar";
+// import Product from "./Routing & Navigation/Pages/Product";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <Home />
+      {/* <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/Home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/About" element={<About />} />
           <Route path="/Product" element={<Product />} />
           <Route path="/Contact" element={<Contact />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
     </>
   );
 }
